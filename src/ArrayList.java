84c76d5d085f4cffeb101f1f9f22bd1b14f11d19
phantom660 <beratamojit660@gmail.com>
2023-03-23 14:24:23 -0500

    Create ArrayList.java

---
+++ src/ArrayList.java
@@ -0,0 +1,46 @@
+public class ArrayList<T extends Comparable<T>> implements List<T> {
+    private T[] arrayList;
+    private boolean isSorted = true;
+    int nextEmpty;
+    public ArrayList() {
+        arrayList = (T[]) new Comparable[2];
+        nextEmpty = 0;
+    }
+
+    private void extendListSize(T[] oldList) {
+        T[] newList = (T[])new Comparable[oldList.length * 2];
+        for (int i = 0; i < oldList.length; i++) {
+            newList[i] = oldList[i];
+        }
+        oldList = newList;
+    }
+
+    @Override
+    public boolean add(T element) {
+        if (element == null) {
+            return false;
+        }
+        if (nextEmpty == arrayList.length) {
+            this.extendListSize(this.arrayList);
+        }
+        arrayList[nextEmpty] = element;
+        if (nextEmpty != 0) {
+            isSorted = (arrayList[nextEmpty].compareTo(arrayList[nextEmpty - 1])) >= 1;
+        }
+        nextEmpty ++;
+        return true;
+    }
+
+    @Override
+    public boolean add(int index, T element) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < arrayList.length; i++) {
+            arrayList[i] = null;
+        }
+        isSorted = true;
+    }
+}
